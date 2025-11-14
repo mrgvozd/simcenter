5a3aef01c7d57992627481704cbcbeb3daf8536a
Nikolay Iminov <43970578+mrgvozd@users.noreply.github.com>
2025-11-14 11:56:06 +0300

    Update studentscript.js

--- site/studentscript.js
+++ site/studentscript.js
@@ -84,7 +84,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	
 
     // Загрузка данных из JSON
-    fetch('../data.json')
+    fetch('data.json')
         .then(response => response.json())
         .then(jsonData => {
             myData = jsonData;
@@ -268,4 +268,5 @@ document.addEventListener('DOMContentLoaded', function () {
 	}
 
 });
+
 
