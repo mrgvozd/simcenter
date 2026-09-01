95eb723986aa2e832a0598f7b26989eb538a1ff9
Nikolay Iminov <43970578+mrgvozd@users.noreply.github.com>
2026-09-01 10:39:20 +0300

    Update studentscript.js

--- site/studentscript.js
+++ site/studentscript.js
@@ -89,7 +89,15 @@ document.addEventListener('DOMContentLoaded', function () {
 			<div class="image-guide">
             <h3>План помещений АСЦ (Коломяжский д. 21, 2 этаж)</h3>
             <div class="guide-image-container">
-                <img src="../Plan-simcenter.png" class="guide-image">
+                <img src="../site/Plan-simcenter.png" class="guide-image">
+			</div>
+			</div>
+		`,
+		nok: `
+			<div class="image-guide">
+            <h3>План помещений НОК (Коломяжский д. 25, 4 корп. 2 этаж)</h3>
+            <div class="guide-image-container">
+                <img src="../site/Plan-nok.png" class="guide-image">
 			</div>
 			</div>
 		`,
@@ -97,7 +105,7 @@ document.addEventListener('DOMContentLoaded', function () {
 			<div class="image-guide">
             <h3>План помещений п. Солнечное, к. 7, 3 этаж </h3>
             <div class="guide-image-container">
-                <img src="../Plan-soln.jpg" class="guide-image">
+                <img src="../site/Plan-soln.jpg" class="guide-image">
 			</div>
 			</div>
 		`
